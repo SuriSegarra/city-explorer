b62c3c1570a370e541c84d9ba8f3073730429089
surelis segarra <sdarianne34@gmail.com>
2020-02-19 14:48:55 -0800

    it will have an api now

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
+require('dotenv').config();
 const express = require('express');
 const app = express ();
 const request = require('superagent');
-const data = require('./geo.js');
+//entonces borras const data porque ya no lo estas usando
+// const data = require('./geo.js');
 const weather = require('./data/darksky.js');
 const cors = require('cors');
+
 
 app.use(cors());
 
 let lat;
 let lng;
 
-app.get('/location', (request, respond) => {
-    const location = request.query.search;
+app.get('/location', async(request, respond, next) => {
+    try {
+    //location sera portland 
+        const location = request.query.search;
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
 
-    const cityData = data.results[0];
-
-lat = cityData.geometry.location.lat;
-lng = cityData.geometry.location.lng;
-
-    respond.json({
-        formatted_query: cityData.formatted_address,
-        latitude:cityData.geometry.location.lat,
-        longitude:cityData.geometry.location.lng,
-    });
+        const cityData = await request.get(URL);
+// makig sure that the get route was working. as i have api, le cambio el valor 
+//antes 
+    // const cityData = data.results[0];
+//despues
+//.body me da todo el data. le puse [0] para ue ,e diera solo el primer item
+        const firstResult = cityData.body[0];
+//luego cambias el valor de estas variables 
+//antes 
+// lat = cityData.geometry.location.lat;
+// lng = cityData.geometry.location.lng;
+//despues 
+        lat = firstResult.lat;
+        lng = firstResult.lon;
+//antes 
+    // respond.json({
+    //     formatted_query: cityData.formatted_address,
+    //     latitude:cityData.geometry.location.lat,
+    //     longitude:cityData.geometry.location.lng,
+//despues 
+        respond.json({
+            formatted_query: firstResult.display_name,
+            latitude: lat,
+            longitude: lng,
+        });
+    } catch (error) {
+        next (error);
+    }
 });
 
 const getWeatherData = (lat, lng) => {
@@ -43,8 +67,9 @@ app.get('/weather', (request, respond) => {
 module.exports = {
     app: app,
 };
-
-const port = process.env.PORT || 5000;
+//SO AQUI ESTAS DICIENDO: usa el port que puse en .env or el que sea que tenga disponible (keep port in the 3000+)
+//si ambos estan ocupados, cambia primero el de env. no el de aqui
+const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
     console.log('listening on port', port);
